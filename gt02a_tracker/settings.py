f67e030b933f4fba740d89be83170abaa20d984f
Roman Lazoryshchak <lazoryshchak@gmail.com>
2016-09-15 18:02:11 +0300

    Update settings.py

--- gt02a_tracker/settings.py
+++ gt02a_tracker/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&c@e5mf8490mx3sz$w^tl6zvhe#s*fkq_@&ny*a(hpn2jqedi)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
